2a1b931a902a626142a9d0f9da9bf31b2a05a2a8
kitai974 <kitai974@hotmail.com>
2016-02-20 00:35:10 +0100

    yeps

--- src/com/hotmail/kitai974/Tempconverter/Launcher.java
+++ src/com/hotmail/kitai974/Tempconverter/Launcher.java
@@ -1,16 +1,40 @@
 package com.hotmail.kitai974.Tempconverter;
 
 import java.util.Scanner;
+import com.hotmail.kitai974.Tempconverter.*;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Temperature a convertire : ");
+		System.out.println("--------------------------------");
+		System.out.println("Convertisseur Celcius Fahrenheit");
+		System.out.println("--------------------------------");
+		System.out.println("Celcius to Fahrenheit tape 1");
+		System.out.println("Fahrenheit to Celcius tape 2");
 		Scanner sc = new Scanner(System.in);
-		String temp = sc.nextLine();
-		
-		System.out.println("La valeur entree est : " + temp);
+		int choise = sc.nextInt();
+		if (choise == 1){
+			System.out.println("Please type the value to convert :");
+			sc.nextLine();
+			double input = sc.nextDouble();			
+			System.out.println(input + " Celcius = " + Fahrenheit(input)+ " Fahrenheit");
+		}		
+		else if (choise == 2){
+			System.out.println("Please type the value to convert :");
+			sc.nextLine();
+			double input = sc.nextDouble();			
+			System.out.println(input + " Fahrenheit = " + Celcius(input)+ " Celcius");
+		}	
 	}
 
+	public static double Celcius(double temp){
+		double result = (temp -32) * (5/9) ;
+		return result;
+	}
+	
+	public static double Fahrenheit(double temp){
+		double result = temp * 9/5 + 32;
+		return result;
+	}
 }
